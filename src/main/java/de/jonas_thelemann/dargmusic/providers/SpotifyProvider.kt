3419a6673a01fbfe4c74ad69b34622902102c510
Jonas Thelemann <e-mail@jonas-thelemann.de>
2019-09-27 02:33:38 +0200

    Spotify: Add Authorization Functionality
    Including UIs.

--- src/main/java/de/jonas_thelemann/dargmusic/providers/SpotifyProvider.kt
+++ src/main/java/de/jonas_thelemann/dargmusic/providers/SpotifyProvider.kt
@@ -3,13 +3,30 @@ package de.jonas_thelemann.dargmusic.providers
 import com.wrapper.spotify.SpotifyApi
 import com.wrapper.spotify.exceptions.SpotifyWebApiException
 import com.wrapper.spotify.exceptions.detailed.NotFoundException
+import com.wrapper.spotify.exceptions.detailed.UnauthorizedException
 import com.wrapper.spotify.model_objects.specification.Playlist
 import com.wrapper.spotify.model_objects.specification.PlaylistTrack
+import de.jonas_thelemann.dargmusic.persistence.state.DargmusicState
 import org.apache.logging.log4j.LogManager
-
+import java.awt.Desktop
 import java.io.IOException
 
+
 object SpotifyProvider : AbstractDargmusicProvider<Playlist, PlaylistTrack>() {
+    val spotifyApiBuilder: SpotifyApi.Builder = SpotifyApi.builder()
+            .setClientId(DargmusicState.settings.spotifySettings.clientId)
+            .setClientSecret(DargmusicState.settings.spotifySettings.clientSecret)
+            .setRedirectUri(DargmusicState.settings.spotifySettings.redirectUri)
+    var spotifyApi: SpotifyApi = spotifyApiBuilder.build()
+
+    init {
+        if (DargmusicState.data.spotifyData.accessToken != "") {
+            spotifyApi = spotifyApiBuilder
+                    .setAccessToken(DargmusicState.data.spotifyData.accessToken)
+                    .build()
+        }
+    }
+
     override fun getPlaylistId(playlist: Playlist): String {
         return playlist.id
     }
@@ -40,5 +57,52 @@ object SpotifyProvider : AbstractDargmusicProvider<Playlist, PlaylistTrack>() {
 
             false
         }
+    }
+
+    fun isValid(): Boolean {
+        if (DargmusicState.data.spotifyData.accessToken == "") {
+            return false
+        }
+
+        var id = String()
+
+        try {
+            id = spotifyApi.currentUsersProfile.build().execute().id
+        } catch (e: UnauthorizedException) {
+            LogManager.getLogger().debug("Access to the Spotify API was unauthorized. Check the access credentials!")
+        }
+
+        return id != ""
+    }
+
+    fun openAuthorization() {
+        val uri = spotifyApi.authorizationCodeUri()
+                .redirect_uri(DargmusicState.settings.spotifySettings.redirectUri)
+//                .scope("playlist-modify-private playlist-read-private user-read-private")
+                .state("dargmusic")
+                .build().execute()
+
+        val os = System.getProperty("os.name").toLowerCase()
+        val runtime = Runtime.getRuntime()
+
+        if (os.contains("mac")) {
+            runtime.exec("open $uri")
+        } else if (os.contains("nix") || os.contains("nux")) {
+            runtime.exec("xdg-open $uri")
+        } else if (Desktop.isDesktopSupported()) {
+            Desktop.getDesktop().browse(uri)
+        }
+    }
+
+    fun setAuthorizationCodeCredentials(authorizationCode: String) {
+        if (authorizationCode == "") {
+            return
+        }
+
+        val authorizationCodeCredentials = spotifyApi.authorizationCode(authorizationCode)
+                .build().execute()
+
+        DargmusicState.data.spotifyData.accessToken = authorizationCodeCredentials.accessToken
+        DargmusicState.data.spotifyData.refreshToken = authorizationCodeCredentials.refreshToken
     }
 }
--- src/main/java/de/jonas_thelemann/dargmusic/ui/controllers/SettingsController.kt
+++ src/main/java/de/jonas_thelemann/dargmusic/ui/controllers/SettingsController.kt
@@ -2,12 +2,20 @@ package de.jonas_thelemann.dargmusic.ui.controllers
 
 import de.jonas_thelemann.dargmusic.persistence.state.settings.spotify.SpotifySettings
 import de.jonas_thelemann.dargmusic.persistence.state.settings.youtube.YouTubeSettings
+import de.jonas_thelemann.dargmusic.providers.SpotifyProvider
+import de.jonas_thelemann.dargmusic.ui.DargmusicStage
 import de.jonas_thelemann.dargmusic.util.Etter
 import javafx.fxml.FXML
 import javafx.fxml.Initializable
+import javafx.scene.control.Button
 import javafx.scene.control.TextField
+import javafx.stage.Modality
+import java.net.MalformedURLException
+import java.net.URI
+import java.net.URISyntaxException
 import java.net.URL
 import java.util.*
+
 
 class SettingsController : Initializable {
 
@@ -16,18 +24,67 @@ class SettingsController : Initializable {
     @FXML
     private lateinit var spotifyClientSecretTextField: TextField
     @FXML
+    private lateinit var spotifyRedirectUriTextField: TextField
+    @FXML
+    private lateinit var btnOpenAuthorization: Button
+    @FXML
     private lateinit var youTubeApiKeyTextField: TextField
 
     override fun initialize(url: URL?, rb: ResourceBundle?) {
-        val inputToEtterMap = mapOf(
+        val uriToEtterMap = mapOf(
+                spotifyRedirectUriTextField to Etter({ SpotifySettings.redirectUri }, { SpotifySettings.redirectUri = it })
+        )
+
+        for ((textField, etter) in uriToEtterMap) {
+            textField.text = etter.getter.invoke().toString()
+            textField.textProperty().addListener { _, _, newText -> etter.setter.invoke(URI(newText)) }
+        }
+
+        val txtToEtterMap = mapOf(
                 spotifyClientIdTextField to Etter({ SpotifySettings.clientId }, { SpotifySettings.clientId = it }),
                 spotifyClientSecretTextField to Etter({ SpotifySettings.clientSecret }, { SpotifySettings.clientSecret = it }),
                 youTubeApiKeyTextField to Etter({ YouTubeSettings.apiKey }, { YouTubeSettings.apiKey = it })
         )
 
-        for ((textField, etter) in inputToEtterMap) {
+        for ((textField, etter) in txtToEtterMap) {
             textField.text = etter.getter.invoke()
             textField.textProperty().addListener { _, _, newText -> etter.setter.invoke(newText) }
+
+            if (textField in setOf(spotifyClientIdTextField, spotifyClientSecretTextField, spotifyRedirectUriTextField)) {
+                textField.textProperty().addListener { _, _, _ ->
+                    btnOpenAuthorization.isDisable = !isAuthorizable()
+                }
+
+                if (textField == spotifyRedirectUriTextField) {
+                    SpotifyProvider.spotifyApi = SpotifyProvider.spotifyApiBuilder.build()
+                }
+            }
         }
+
+        btnOpenAuthorization.isDisable = !isAuthorizable()
+    }
+
+    private fun isAuthorizable(): Boolean {
+        if (spotifyClientIdTextField.text == ""
+                || spotifyClientSecretTextField.text == ""
+                || spotifyRedirectUriTextField.text == "") {
+            return false
+        }
+
+        return try {
+            URL(spotifyRedirectUriTextField.text).toURI()
+            true
+        } catch (exception: URISyntaxException) {
+            false
+        } catch (exception: MalformedURLException) {
+            false
+        }
+    }
+
+    @FXML
+    private fun openAuthorization() {
+        SpotifyProvider.openAuthorization()
+        DargmusicStage("../fxml/spotify/SpotifyAuthorizationCode.fxml", Modality.APPLICATION_MODAL, "Authorization Code")
+                .showStyled()
     }
 }

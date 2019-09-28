c2fabc5b606d951a844225117569b3d55e29e34c
Jonas Thelemann <e-mail@jonas-thelemann.de>
2019-09-28 21:24:41 +0200

    Models: Separate PlaylistMappingResource

--- src/main/java/de/jonas_thelemann/dargmusic/models/PlaylistMapping.kt
+++ src/main/java/de/jonas_thelemann/dargmusic/models/PlaylistMapping.kt
@@ -2,24 +2,20 @@ package de.jonas_thelemann.dargmusic.models
 
 import de.jonas_thelemann.dargmusic.models.enums.DargmusicProvider
 import de.jonas_thelemann.dargmusic.persistence.state.DargmusicState
-import de.jonas_thelemann.dargmusic.providers.FileSystemProvider
-import de.jonas_thelemann.dargmusic.providers.SpotifyProvider
 import de.jonas_thelemann.dargmusic.util.Util
 
-data class PlaylistMapping(var name: String = Util.getUnusedPlaylistMappingName(DargmusicState.data.playlistMappingData.playlistMappings),
-                           var sourceProvider: DargmusicProvider = DargmusicProvider.NONE,
-                           var sourceId: String = String(),
-                           var targetProvider: DargmusicProvider = DargmusicProvider.NONE,
-                           var targetId: String = String(),
+data class PlaylistMapping(var name: String = Util.getUnusedPlaylistMappingName(DargmusicState.data.playlistMappings),
+                           var sourceResource: PlaylistMappingResource = PlaylistMappingResource(),
+                           var targetResource: PlaylistMappingResource = PlaylistMappingResource(),
                            var blacklistSource: Array<String> = arrayOf(),
                            var blacklistTarget: Array<String> = arrayOf()) {
 
     fun validate(): Boolean {
         return name != ""
-                && sourceProvider != DargmusicProvider.NONE
-                && DargmusicProvider.isIdValid(sourceId, sourceProvider)
-                && targetProvider != DargmusicProvider.NONE
-                && DargmusicProvider.isIdValid(targetId, targetProvider)
+                && sourceResource.provider != DargmusicProvider.NONE
+                && DargmusicProvider.isValid(sourceResource)
+                && targetResource.provider != DargmusicProvider.NONE
+                && DargmusicProvider.isValid(targetResource)
     }
 
     override fun equals(other: Any?): Boolean {
@@ -29,10 +25,8 @@ data class PlaylistMapping(var name: String = Util.getUnusedPlaylistMappingName(
         other as PlaylistMapping
 
         if (name != other.name) return false
-        if (sourceProvider != other.sourceProvider) return false
-        if (targetProvider != other.targetProvider) return false
-        if (sourceId != other.sourceId) return false
-        if (targetId != other.targetId) return false
+        if (sourceResource != other.sourceResource) return false
+        if (targetResource != other.targetResource) return false
         if (!blacklistSource.contentEquals(other.blacklistSource)) return false
         if (!blacklistTarget.contentEquals(other.blacklistTarget)) return false
 
@@ -41,10 +35,8 @@ data class PlaylistMapping(var name: String = Util.getUnusedPlaylistMappingName(
 
     override fun hashCode(): Int {
         var result = name.hashCode()
-        result = 31 * result + sourceProvider.hashCode()
-        result = 31 * result + targetProvider.hashCode()
-        result = 31 * result + sourceId.hashCode()
-        result = 31 * result + targetId.hashCode()
+        result = 31 * result + sourceResource.hashCode()
+        result = 31 * result + targetResource.hashCode()
         result = 31 * result + blacklistSource.contentHashCode()
         result = 31 * result + blacklistTarget.contentHashCode()
         return result

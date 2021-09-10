8a79d78c0662f48ed2ddcf07a43ff56c1ed31cc8
Jonas Thelemann <e-mail@jonas-thelemann.de>
2021-09-10 07:45:22 +0200

    fix(module): correct info location

--- src/main/java/de/dargmuesli/module-info.java
+++
@@ -1,1 +0,0 @@
-module de.dargmuesli.dargmusic { }
---
+++ src/module-info.java
@@ -0,0 +1,11 @@
+module de.dargmuesli.dargmusic {
+    requires org.apache.logging.log4j;
+    requires spotify.web.api.java;
+    requires kotlin.stdlib;
+    requires com.fasterxml.jackson.databind;
+    requires com.google.api.services.youtube;
+    requires com.google.api.client;
+    requires com.google.api.client.json.jackson2;
+    requires java.logging;
+    requires java.desktop;
+}
